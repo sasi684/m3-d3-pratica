b9a6f51538e2fa47af7adffa6dc317a9dbfaec14
Salvatore Triunfo <sasi684@hotmail.com>
2025-11-18 20:19:50 +0100

    Svolto esercizio 1

---
+++ Assets/_Project/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpStrength;
+    [SerializeField] private Rigidbody2D _rb;
+
+    private float h;
+    private bool _isJumping;
+
+    void Start()
+    {
+        if( _rb == null )
+            _rb = GetComponent<Rigidbody2D>();
+        if (_rb == null)
+            Debug.LogError($"Nessuna componente RigidBody2D trovata per l'oggetto {gameObject.name}");
+    }
+
+    private void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _isJumping = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(h * _speed, _rb.velocity.y);
+        _rb.velocity = direction;
+
+        if (_isJumping)
+        {
+            _rb.AddForce(Vector2.up * _jumpStrength, ForceMode2D.Impulse);
+            _isJumping = false;
+        }
+    }
+}
